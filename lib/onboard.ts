1876e17c5af9294aa7f8f4757361c7109e7a4db1
SH701 <rkrk7636@naver.com>
2025-08-03 20:25:09 +0900

    온보딩 멘트 주세요

--- lib/onboard.ts
+++ lib/onboard.ts
@@ -12,20 +12,20 @@
     {
       id: 1,
       img: "/slide1.png",
-      title: "한국의 숨은 표현 배우기",
-      desc: "일상 속에서 쓰이는 한국어 문화 표현을 알려드립니다.",
+      title: "온보딩",
+      desc: "온보딩",
     },
     {
       id: 2,
       img: "/slide2.png",
-      title: "쉽고 재미있게",
-      desc: "그림과 예시로 직관적으로 배워보세요.",
+      title: "온보딩",
+      desc: "온보딩",
     },
     {
       id: 3,
       img: "/slide3.png",
-      title:"설명",
-      desc: "",
+      title:"온보딩",
+      desc: "온보딩",
       isFinal:true
     }
   ];
