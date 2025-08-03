898aedaebf7edbb86a052ecc7a03c171bab7ee58
SH701 <rkrk7636@naver.com>
2025-08-03 18:32:48 +0900

    시작페이지 변경

---
+++ components/onboard.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Image from "next/image";
+import { settings, slides } from "@/lib/onboard";
+import Link from "next/link";
+import "@/style/button.css"
+
+export default function Onboard() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-white">
+      <div className="w-full max-w-md">
+        <Slider {...settings}>
+          {slides.map((slide) => (
+            <div key={slide.id}>
+              <div className="flex flex-col items-center justify-center text-center p-6 min-h-[70vh]">
+                <div className="flex-shrink-0 mb-4">
+                  <Image
+                    src={slide.img}
+                    alt={slide.title}
+                    width={200}
+                    height={200}
+                    style={{ objectFit: "contain" }}
+                    priority
+                  />
+                </div>
+                <h2 className="text-2xl font-bold mt-2">{slide.title}</h2>
+                <p className="text-gray-600 mt-1">{slide.desc}</p>
+                <Link href="/first" className="btn pageTitle">
+                    <span className="text-sm text-white">{slide.start}</span>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+}
