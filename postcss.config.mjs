f8801c93d2092f9beb722aba4d55bb4468e27dd2
minchulpack <thqnfos33@gmail.com>
2025-08-13 22:25:39 +0900

    Fix postcss.config.mjs to original state

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,26 +1,5 @@
-import { tailwindcss } from '@tailwindcss/postcss';
-import { nextPWA } from 'next-pwa';
-
-export default {
-  plugins: [
-    tailwindcss,
-    nextPWA({
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-      disable: process.env.NODE_ENV === 'development',
-      runtimeCaching: [
-        {
-          urlPattern: /^https?.*/,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'offlineCache',
-            expiration: {
-              maxEntries: 200,
-            },
-          },
-        },
-      ],
-    }),
-  ],
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
